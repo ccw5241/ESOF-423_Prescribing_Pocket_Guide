31fe61d18e4318364767be0973c4e9aaa740f554
Chenwei Cao <ccw5241@gmail.com>
2018-03-23 08:33:24 -0600

    add travisTest

---
+++ group3/assets/js/testTravis.js
@@ -0,0 +1,13 @@
+  var cookies = require('cookies');
+  var createID = require('createID');
+  var loadPatien = require('loadPatients');
+  var log = require('longin');
+  var mdqdiag = require('MDQDiag');
+  var modal = require('modalPerson');
+  var patient = require('Patient');
+  var ph = require('PHQ9Diag');
+  var test = require('test');
+
+  testTravis.task('travis',['build','testServerJS'], function(){
+    process.exit(0);
+  });
--- group3/assets/js/travisTest.js
+++
@@ -1,3 +0,0 @@
-function test(input){
-	return input+1;
-}

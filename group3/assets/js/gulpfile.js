05df50aaae27b3b1fad36c4ecbc3139548cd8f02
Chenwei Cao <ccw5241@gmail.com>
2018-03-27 18:54:42 -0600

    set up travis

--- group3/assets/js/gulpfile.js
+++ group3/assets/js/gulpfile.js
@@ -1,13 +1,16 @@
-  var gulp = require('gulp');
-  var babel = require('gulp-babel');
-  var runSeq = require('run-sequence');
-  var plumber = require('gulp-plumber');
-  var concat = require('gulp-concat');
-  var rename = require('gulp-rename');
-  var sass = require('gulp-sass');
-  var livereload = require('gulp-livereload');
-  var test = require('test');
+  var gulp = require('group3/assets/js/.js');
+  var cookie = require('group3/assets/js/cookies.js');
+  var creatid = require('group3/assets/js/creatID.js');
+  var mdqd = require('group3/assets/js/MDQDiag.js');
+  var modalPerson = require('group3/assets/js/modalPerson.js');
+  var pack = require('group3/assets/js/package.js');
+  var pat = require('group3/assets/js/Patient.js');
+  var phq = require('group3/assets/js/PHQ9Diag.js');
 
   gulp.task('travis',['build','testServerJS'], function(){
     process.exit(0);
   });
+
+  cookie.task('travis',['build','testServerJS'], getCookieDataByKey("he"){
+    process.exit(0);
+  })
